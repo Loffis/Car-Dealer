4f12d2b1ca5f1f187b92a78012a92a8d88a20fdf
Loffe Knutsson <loffe.knutsson@gmail.com>
2020-10-29 12:23:29 +0100

    Init

---
+++ src/main/java/se/ecutb/cardealer/CarDealerApiApplication.java
@@ -0,0 +1,13 @@
+package se.ecutb.cardealer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarDealerApiApplication.class, args);
+    }
+
+}

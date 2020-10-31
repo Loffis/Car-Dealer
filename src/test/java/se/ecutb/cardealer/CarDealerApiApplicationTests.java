48df64c83a78e1574e78732d25b8f86595863433
Loffe Knutsson <loffe.knutsson@gmail.com>
2020-10-31 01:11:58 +0100

    commiting unversioned files.

---
+++ src/test/java/se/ecutb/cardealer/CarDealerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.ecutb.cardealer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDealerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

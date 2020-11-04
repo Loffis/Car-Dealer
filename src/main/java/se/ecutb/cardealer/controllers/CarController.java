bba349417e2182fc0d523f3c5fd64c8d28245f98
jkpoea <jkpeoa@gmail.com>
2020-11-04 22:19:19 +0100

    Comments

--- src/main/java/se/ecutb/cardealer/controllers/CarController.java
+++ src/main/java/se/ecutb/cardealer/controllers/CarController.java
@@ -3,7 +3,6 @@ package se.ecutb.cardealer.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import se.ecutb.cardealer.entities.Car;
@@ -18,57 +17,57 @@ public class CarController {
     @Autowired
     private CarService carService;
 
-    @Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
+    //@Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
     @GetMapping
     public ResponseEntity<List<Car>> findAllCars(@RequestParam(required = false) String name,
                                                  @RequestParam(required = false) boolean sort){
         return ResponseEntity.ok(carService.findAll(name, sort));
     }
 
-    @Secured({"ROLE_DEALER", "ROLE_ADMIN"})
+    //@Secured({"ROLE_DEALER", "ROLE_ADMIN"})
     @GetMapping("/{id}")
     public ResponseEntity<Car> findCarById(@PathVariable String id){
         return ResponseEntity.ok(carService.findbyId(id));
     }
 
-    @Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
+    //@Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
     @GetMapping("/status={status}")
     public ResponseEntity<List<Car>> findCarsByStatus(@PathVariable String status){
         return ResponseEntity.ok(carService.findByStatus(status));
     }
 
-    @Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
+    //@Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
     @GetMapping("/brand={brand}")
     public ResponseEntity<List<Car>> findCarsByBrand(@PathVariable String brand){
         return ResponseEntity.ok(carService.findByBrand(brand));
     }
 
-    @Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
+    //@Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
     @GetMapping("/model={model}")
     public ResponseEntity<Car> findCarByModel(@PathVariable String model){
         return ResponseEntity.ok(carService.findByModel(model));
     }
 
-    @Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
+    //@Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
     @GetMapping("/seats={seats}")
     public ResponseEntity<List<Car>> findCarsBySeats(@PathVariable int seats){
         return ResponseEntity.ok(carService.findBySeats(seats));
     }
 
-    @Secured({"ROLE_DEALER", "ROLE_ADMIN"})
+    //@Secured({"ROLE_DEALER", "ROLE_ADMIN"})
     @PostMapping
     public ResponseEntity<Car> saveCar(@Validated @RequestBody Car car){
         return ResponseEntity.ok(carService.save(car));
     }
 
-    @Secured({"ROLE_DEALER", "ROLE_ADMIN"})
+    //@Secured({"ROLE_DEALER", "ROLE_ADMIN"})
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void updateCar(@PathVariable String id, @Validated @RequestBody Car car){
         carService.update(id, car);
     }
 
-    @Secured({"ROLE_DEALER", "ROLE_ADMIN"})
+    //@Secured({"ROLE_DEALER", "ROLE_ADMIN"})
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteCar(@PathVariable String id){
--- src/main/java/se/ecutb/cardealer/controllers/WheelsController.java
+++ src/main/java/se/ecutb/cardealer/controllers/WheelsController.java
@@ -3,7 +3,6 @@ package se.ecutb.cardealer.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import se.ecutb.cardealer.entities.Wheels;
@@ -18,68 +17,68 @@ public class WheelsController {
     @Autowired
     private WheelsService wheelsService;
 
-    @Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
+    //@Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
     @GetMapping
     public ResponseEntity<List<Wheels>> findAllWheels(){
         return ResponseEntity.ok(wheelsService.findAll());
     }
 
-    @Secured({"ROLE_DEALER", "ROLE_ADMIN"})
+    //@Secured({"ROLE_DEALER", "ROLE_ADMIN"})
     @GetMapping("/{id}")
     public ResponseEntity<Wheels> findWheelsById(@PathVariable String id){
         return ResponseEntity.ok(wheelsService.findById(id));
     }
 
-    @Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
+    //@Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
     @GetMapping("/rimMaterial={type}")
     public ResponseEntity<List<Wheels>> findWheelsByRimMaterial(@PathVariable String type){
         return ResponseEntity.ok(wheelsService.findByRimMaterial(type));
     }
 
-    @Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
+    //@Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
     @GetMapping("/rimColor={color}")
     public ResponseEntity<List<Wheels>> findWheelsByRimColor(@PathVariable String color) {
         return ResponseEntity.ok(wheelsService.findByRimColor(color));
     }
 
-    @Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
+    //@Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
     @GetMapping("/rimSize={size}")
     public ResponseEntity<List<Wheels>> findWheelsByRimSize(@PathVariable int size) {
         return ResponseEntity.ok(wheelsService.findByRimSize(size));
     }
 
-    @Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
+    //@Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
     @GetMapping("/tireWidth={width}")
     public ResponseEntity<List<Wheels>> findWheelsByTireWidth(@PathVariable int width){
         return ResponseEntity.ok(wheelsService.findByTireWidth(width));
     }
 
-    @Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
+    //@Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
     @GetMapping("/tireProfile={profile}")
     public ResponseEntity<List<Wheels>> findWheelsByTireProfile(@PathVariable int profile){
         return ResponseEntity.ok(wheelsService.findByTireProfile(profile));
     }
 
-    @Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
+    //@Secured({"ROLE_USER", "ROLE_DEALER", "ROLE_ADMIN"})
     @GetMapping("/tireSpeedRating={rating}")
     public ResponseEntity<Wheels> findWheelsByTireSpeedRating(@PathVariable char rating){
         return ResponseEntity.ok(wheelsService.findByTireSpeedRating(rating));
     }
 
-    @Secured({"ROLE_DEALER", "ROLE_ADMIN"})
+    //@Secured({"ROLE_DEALER", "ROLE_ADMIN"})
     @PostMapping
     public ResponseEntity<Wheels> saveWheels(@Validated @RequestBody Wheels wheels){
         return ResponseEntity.ok(wheelsService.save(wheels));
     }
 
-    @Secured({"ROLE_DEALER", "ROLE_ADMIN"})
+    //@Secured({"ROLE_DEALER", "ROLE_ADMIN"})
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void updateWheels(@PathVariable String id, @Validated @RequestBody Wheels wheels){
         wheelsService.update(id, wheels);
     }
 
-    @Secured({"ROLE_DEALER", "ROLE_ADMIN"})
+    //@Secured({"ROLE_DEALER", "ROLE_ADMIN"})
     @DeleteMapping("/{id}")
     public void deleteWheels(@PathVariable String id){
         wheelsService.delete(id);
